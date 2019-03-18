ec7485520faa152f706d73b6304686ec1c3e49d7
小罗 <1071865953@qq.com>
2019-03-18 11:17:47 +0800

    初始化版本

---
+++ mysite/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('center/', views.dashboard, name='dashboard'),
+    path('register/',views.register,name='register'),
+    path('', include('django.contrib.auth.urls')),
+
+    path('profiles/', views.ProfileList.as_view(), name='pro_list'),
+    path('profile/edit/', views.profileedit, name='pro_edit'),
+    path('profile/<pk>/', views.ProfileDetail.as_view(), name='pro_detail'),
+
+    path('like/', views.user_like, name='like'),
+    path('user_chk/',views.user_chk,name='user_chk'),
+
+    path('posts/<int:id>/',views.catposts,name='posts_by_cat'),
+    path('post/create/',views.PostCreate.as_view(),name='post_create'),
+    path('post/comment/',views.post_comment,name='post_comment'),
+    path('post/<year>/<month>/<day>/<slug>/',views.PostDetail.as_view(),name='post_detail'),
+]
+
